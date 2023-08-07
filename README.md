668dbc41dc7aa88f45cfaf3a0aa7e6c08acb124f
Himanshu9604 <111119866+Himanshu9604@users.noreply.github.com>
2023-08-07 19:17:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe-Game
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-xs1u2z)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+import Board from './tic-tac-toe-game/Board'
+export default function App() {
+  return (
+    <div>
+     <Board />
+    </div>
+  );
+}
